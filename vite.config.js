c66a8702ed1d31e6dc1b64712b94576650d6c7cb
Alex <toer1xe@gmail.com>
2025-07-22 11:34:59 +0200

    gh_pages base

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/stackers_/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
